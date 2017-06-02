e2182fde80b63a12e4c8eb6928244f44e820d553
joblogmans <job@logmans.eu>
2017-06-02 11:21:53 +0200

    Created Sidebar.php
    This is the sidebar used on most of the pages, include in php using require() or include()

---
+++ Sidebar.php
@@ -0,0 +1,17 @@
+<nav class="w3-sidebar w3-bar-block w3-card w3-collapse w3-animate-left" style="display:none; z-index:4" id="Sidebar">
+  <div class="w3-container w3-theme-d2">
+    <span onclick="closeSidebar()" class="w3-button w3-display-topright w3-large">X</span>
+    <br>
+    <div class="w3-padding w3-center">
+      <img class="w3-circle" src="Images/home.png" alt="avatar" style="width:75%">
+    </div>
+  </div>
+  <a class="w3-bar-item w3-button" href="MainPage.php">Home</a>
+  <a class="w3-bar-item w3-button" href="Inventory.php">Inventory</a>
+  <a class="w3-bar-item w3-button" href="Spells.php">Spells</a>
+  <a class="w3-bar-item w3-button" href="Wiki.php">Wiki</a>
+  <a class="w3-bar-item w3-button" href="Character.php">Character stats</a>
+  <a class="w3-bar-item w3-button" href="#">Your Characters</a>
+  <a class="w3-bar-item w3-button" href="#">Your account</a>
+  <a class="w3-bar-item w3-button" href="#">Logout</a>
+</nav>
