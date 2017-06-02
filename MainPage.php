088220fac3578b8a85f6551760b0bc3f8eb44673
joblogmans <job@logmans.eu>
2017-06-02 11:35:35 +0200

    Added page title variable with php
    Edit $PageTitle to edit the title on all pages.

--- MainPage.php
+++ MainPage.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
+//Set the page title used on this page, edit this to edit it everywhere
+<?php $PageTitle= "Home"; ?>
 <html>
   <head>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>D&D Main Page</title>
+    <?php 
+      echo "<title>$PageTitle</title>"; 
+     ?>
   </head>
   <body>
   </body>
