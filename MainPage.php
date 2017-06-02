66b40e5b35e8db532390a161e6c8a538144d6cc1
joblogmans <job@logmans.eu>
2017-06-02 11:17:02 +0200

    Update MainPage.php
    Added everything to the <head> tag (view port, w3.css style sheet, page title)

--- MainPage.php
+++ MainPage.php
@@ -1,1 +1,10 @@
-<html></html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>D&D Main Page</title>
+  </head>
+  <body>
+  </body>
+</html>
